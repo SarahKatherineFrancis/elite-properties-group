c566544dbbbda1d6ce7d821c5e9b5b059450e30f
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-07-24 13:23:45 +0200

    added popular properties slider with Swiper

--- js/main.js
+++ js/main.js
@@ -8,3 +8,16 @@ function scrollHeader() {
 }
 
 window.addEventListener("scroll", scrollHeader);
+
+// Swiper popular
+var swiperPopular = new Swiper(".popular__container", {
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
